15122693bee83f2f798ce6f0d7ff373ba721597d
drubaby <peryton6@gmail.com>
2018-08-30 17:56:46 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+ attr_accessor :sample
+
+ def initialize(sample)
+   @sample = sample
+ end
+
+ def match(array)
+   array.select do |word|
+     word.split("").sort == @sample.split("").sort
+   end
+ end
+
+
+
+
+end
