9228669f7d22318457937240e69d599effe0edd1
jb5595 <jacobberman1995@gmail.com>
2018-08-29 21:08:02 -0400

    completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :base_word
+  def initialize(word)
+    @base_word = word
+  end
+
+  def match(array_of_words)
+    @anagrams = []
+    array_of_words.each do |word|
+      if @base_word.split("").sort == word.split("").sort
+        @anagrams << word
+      end
+    end
+    @anagrams
+  end
+end
