d2f1205f343fdd5ad61f07c4f0ad21445ba3ccd1
github username <github email address>
2018-08-30 17:55:08 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(random)
+    arr = []
+    random.each do |element|
+      if @word.split("").sort == element.split("").sort
+        arr << element
+      end
+    end
+    arr
+  end
+
+end
