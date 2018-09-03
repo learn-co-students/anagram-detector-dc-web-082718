dda08811a8a4127e45018fc4418998602f44fe8a
samashtar <sam.ashtar@flatironschool.com>
2018-09-02 22:21:13 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word_to_analyze, :array
+
+def initialize (word_to_analyze)
+@word_to_analyze = word_to_analyze
+
+
+end
+
+def match (array)
+array.find_all do |word|
+if word.split("").sort == self.word_to_analyze.split("").sort
+       word
+
+     end
+   end
+
+
+
+end
+
+
+
+
+
+
+
+
+
+end
