eca70b3dec78205aba06d65b0e5f4843cb91a2c2
Trevor Jameson <github email address>
2018-09-04 15:12:03 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(match_words)
+    match_words.collect do |match_word|
+      if sorter(match_word) == sorter(@word)
+        match_word
+      end
+    end.compact
+  end
+
+  # Accepts a word and returns a sorted array of letters
+  def sorter(sortable_word)
+    sortable_word.split("").sort
+  end
+end
